e74cd610f7098b514826bd7faa80ee96ff38cee5
Nour <mohamednour25@gmail.com>
2024-06-09 11:58:12 +0300

    Initial Commit

---
+++ NourCard/NourCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  NourCardApp.swift
+//  NourCard
+//
+//  Created by administrator on 09/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct NourCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
