332dd0f2c1e91620abab782361106606d67a0466
Nour <mohamednour25@gmail.com>
2024-06-23 21:31:09 +0300

    01

---
+++ NourCard/InfoView.swift
@@ -0,0 +1,18 @@
+//
+//  InfoView.swift
+//  NourCard
+//
+//  Created by administrator on 09/06/2024.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoView()
+}
